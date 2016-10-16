ea2dc3397534b1da2473985ba7076a1d133610de
vaniok123 <smirnovivan@macbook-pro-smirnov.local>
2016-10-16 21:53:52 +0300

    Initial Commit

---
+++ 500PXapp/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  500PXapp
+//
+//  Created by Smirnov Ivan on 16.10.16.
+//  Copyright © 2016 techpark_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ 500PXapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  500PXapp
+//
+//  Created by Smirnov Ivan on 16.10.16.
+//  Copyright © 2016 techpark_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
