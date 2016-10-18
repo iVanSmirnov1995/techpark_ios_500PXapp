6ab51a58d5b9fb49a1168fdb760bd195a782dd86
vaniok123 <smirnovivan@macbook-pro-smirnov.local>
2016-10-18 14:15:34 +0300

    add  UITableViewCell custom class

---
+++ 500PXapp/ISTableViewCommentsCell.h
@@ -0,0 +1,13 @@
+//
+//  ISTableViewCommentsCell.h
+//  500PXapp
+//
+//  Created by Smirnov Ivan on 18.10.16.
+//  Copyright © 2016 techpark_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISTableViewCommentsCell : UITableViewCell
+
+@end
---
+++ 500PXapp/ISTableViewUserInfoCell.m
@@ -0,0 +1,24 @@
+//
+//  ISTableViewUserInfoCell.m
+//  500PXapp
+//
+//  Created by Smirnov Ivan on 18.10.16.
+//  Copyright © 2016 techpark_ios. All rights reserved.
+//
+
+#import "ISTableViewUserInfoCell.h"
+
+@implementation ISTableViewUserInfoCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
