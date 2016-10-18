2a279919daaf33fc1ead24a633f95d9e18db5de3
vaniok123 <smirnovivan@macbook-pro-smirnov.local>
2016-10-18 12:19:34 +0300

    add TableView in home vc

---
+++ 500PXapp/ISHomeVC.m
@@ -0,0 +1,59 @@
+//
+//  ISHomeVC.m
+//  500PXapp
+//
+//  Created by Smirnov Ivan on 18.10.16.
+//  Copyright © 2016 techpark_ios. All rights reserved.
+//
+
+#import "ISHomeVC.h"
+
+@interface ISHomeVC ()<UITableViewDataSource,UITableViewDelegate>
+
+@end
+
+@implementation ISHomeVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark-UITableViewDataSource
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    
+    return 1;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    
+    return 1;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    return nil;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
