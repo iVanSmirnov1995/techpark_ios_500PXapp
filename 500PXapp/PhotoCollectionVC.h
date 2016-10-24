8bcf66c5cb1038ce0784abafe339ca1646a3bf09
user <user@macbook-pro-user.local>
2016-10-24 18:35:19 +0300

    add Class for PhotosCollection

---
+++ 500PXapp/PhotoCollectionVC.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionVC.h
+//  500PXapp
+//
+//  Created by user on 24.10.16.
+//  Copyright © 2016 techpark_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCollectionVC : UICollectionViewController
+
+@end
---
+++ PhotosCellModel.h
@@ -0,0 +1,13 @@
+//
+//  PhotosCellModel.h
+//  500PXapp
+//
+//  Created by user on 24.10.16.
+//  Copyright © 2016 techpark_ios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PhotosCellModel : NSObject
+
+@end
