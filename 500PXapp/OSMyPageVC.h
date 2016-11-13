8c723ea6fcdf0395f68447cf2efb040026245ce8
user <user@h246.net48.bmstu.ru>
2016-11-13 13:44:06 +0300

    init2

---
+++ 500PXapp/OSMyPageVC.h
@@ -0,0 +1,16 @@
+//
+//  OSMyPageVC.h
+//  500PXapp
+//
+//  Created by user on 13.11.16.
+//  Copyright © 2016 techpark_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OSMyPageVC : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *miniature;
+@property (weak, nonatomic) IBOutlet UIImageView *avatar;
+
+@end
